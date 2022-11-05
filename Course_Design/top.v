90b32c811aa111cf469987cff60cfdff4a1f2c90
Fallere-825 <wuyu825@gmail.com>
2022-11-05 19:39:33 +0800

    修改顶层模块

--- Course_Design/top.v
+++ Course_Design/top.v
@@ -1,48 +1,34 @@
 `timescale 1ns/1ps
 
-module top (output wire [63:0] data_out_64,
-            input wire clk,
-            input wire rst_n,
-            input wire manual_start,
-            input wire [63:0] data_in_64);
+/* 顶层模块 */
 
+module top (output wire [63:0] data_out_64, // 输出64位并行数据
+            output wire data_out_done,      // 全部64位数据传输完成标志
+            input wire clk,                 // 系统时钟
+            input wire rst_n,               // 复位信号，低电平有效
+            input wire manual_start,        // 手动启动系统信号
+            input wire [63:0] data_in_64);  // 输入64位并行数据
 
-wire uart_txd;
-
-wire rst_check;
-
-wire rst_total;
-
-reg   [63:0] data_in_1;
-
-assign rst_check = (data_in_64 == data_in_1)? 1'b1 : 1'b0;
-
-assign rst_total = ~(~(rst_check) || ~(rst_n));
-
-always @(posedge clk) begin
-    
-    data_in_1 <= data_in_64;
-    
-    
-end
-
-
+/* 定义线网 */
+wire data; // tx传输给rx的1位数据
 
 top_in  u_top_in (
 .clk                     (clk),
 .rst_n                   (rst_n),
-.manual_start            (manual_start),
 .data_64                 (data_in_64),
+.manual_start            (manual_start),
 
-.uart_txd                (uart_txd)
+.uart_txd                (data)
 );
 
 
 top_out  u_top_out (
 .clk                     (clk),
 .rst_n                   (rst_n),
-.uart_txd                (uart_txd),
+.uart_rxd                (data),
 
-.data_64                 (data_out_64)
+.data_64                 (data_out_64),
+.data_out_done           (data_out_done)
 );
+
 endmodule // top
--- Course_Design/top_out.v
+++ Course_Design/top_out.v
@@ -1,28 +1,25 @@
 `timescale 1ns/1ps
 
-module top_out (output wire [63:0] data_64,
-                input wire clk,
-                input wire rst_n,
-                input wire uart_txd);
+/* 串行接收 与 并行64位数据输出 模块 */
 
+module top_out (output wire [63:0] data_64, // 输出64位并行数据
+                output wire data_out_done,  // 全部64位数据传输完成标志
+                input wire clk,             // 系统时钟
+                input wire rst_n,           // 复位信号，低电平有效
+                input wire uart_rxd);       // UART接收端口
 
-wire [7:0] uart_data_out;
-wire uart_done;
+/* 定义线网 */
+wire [7:0] uart_data_out; // 【rx】传给【8位转64位模块】的8位并行数据
+wire uart_done;           // rx数据接收完成标志，让8位转64位模块读入新到的一组8位数据
 
-
-
-
-uart_rx
-u_uart_rx (
+uart_rx  u_uart_rx (
 .clk                     (clk),
 .rst_n                   (rst_n),
-.uart_rxd                (uart_txd),
+.uart_rxd                (uart_rxd),
 
 .uart_data_out           (uart_data_out),
 .uart_done               (uart_done)
 );
-
-
 
 data_out_8_to_64  u_data_out_8_to_64 (
 .clk                     (clk),
@@ -30,7 +27,8 @@ data_out_8_to_64  u_data_out_8_to_64 (
 .data_8                  (uart_data_out),
 .data_out_enable         (uart_done),
 
-.data_64                 (data_64)
+.data_64                 (data_64),
+.data_out_done           (data_out_done)
 );
 
 endmodule // top_out
